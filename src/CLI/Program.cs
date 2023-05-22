66421eda43b72121f33c8326f9dadecd4a9dcf03
Paolo Ambrosio <git@paolo.ambrosio.name>
2023-05-22 08:28:14 +0100

    Config from file or command line

--- src/CLI/Program.cs
+++ src/CLI/Program.cs
@@ -1,8 +1,18 @@
 ﻿using Core;
+using Microsoft.Extensions.Configuration;
 
 try
 {
-    ModManager.Init().InstallEnabledMods();
+    var config = new ConfigurationBuilder()
+        .AddIniFile("AMS2CM.ini")
+        .AddCommandLine(args)
+        .Build();
+    var gameConfig = config
+        .GetSection("Game")
+        .Get<ModManager.GameConfig>() ?? throw new Exception("Failed to read the configuration file");
+    var modManagerConfig = new ModManager.Config(Game: gameConfig);
+    var modManager = ModManager.Init(modManagerConfig);
+    modManager.InstallEnabledMods();
 }
 catch (Exception e)
 {
