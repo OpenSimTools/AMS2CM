25db6847c8039fea55001d2e8c3dfb9cb966bec0
Paolo Ambrosio <git@paolo.ambrosio.name>
2024-05-06 07:20:46 +0100

    Better messaging for Steam errors (#119)

--- src/Core/Games/Steam.cs
+++ src/Core/Games/Steam.cs
@@ -1,6 +1,8 @@
-﻿using Gameloop.Vdf;
+﻿using Core.Utils;
+using Gameloop.Vdf;
 using Gameloop.Vdf.JsonConverter;
 using Microsoft.Win32;
+using System.IO;
 
 namespace Core.Games;
 
@@ -11,21 +13,32 @@ public static class Steam
         @"HKEY_LOCAL_MACHINE\SOFTWARE\Valve\Steam"
     };
 
-    public static string? MainInstallationDirectory()
+    public static string MainInstallationPath()
     {
-        return RegistryPaths.Select(registryPath =>
-            Registry.GetValue(registryPath, "InstallPath", null) as string
-        ).FirstOrDefault(path => path != null && Directory.Exists(path));
+        try {
+            return RegistryPaths
+                .SelectNotNull(registryPath =>
+                    Registry.GetValue(registryPath, "InstallPath", null) as string
+                ).Single(path => Directory.Exists(path));
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Cannot find Steam installation path", e);
+        }
     }
 
-    public static string? AppLibraryPath(string appId)
+    public static string AppLibraryPath(string appId)
     {
-        var steamDir = MainInstallationDirectory();
-        if (steamDir == null)
-            return null;
+        var steamDir = MainInstallationPath();
         var libraryFoldersPath = Path.Combine(steamDir, "steamapps", "libraryfolders.vdf");
-        var libraryFolders = ReadLibraryFolders(libraryFoldersPath);
-        return libraryFolders.SingleOrDefault(lf => lf.Apps.ContainsKey(appId))?.Path;
+        var gameLibraryFolders = ReadLibraryFolders(libraryFoldersPath)
+            .Where(lf => lf.Apps.ContainsKey(appId));
+        return gameLibraryFolders.Count() switch
+        {
+            0 => throw new Exception($"Cannot find app ID {appId} in any Steam library"),
+            1 => gameLibraryFolders.Single().Path,
+            var count => throw new Exception($"App ID {appId} found in {count} Steam libraries")
+        };
     }
 
     private static IEnumerable<LibraryFolder> ReadLibraryFolders(string path)
@@ -33,8 +46,8 @@ public static class Steam
         var libraryFolders = File.ReadAllText(path);
         var vdfRoot = VdfConvert.Deserialize(libraryFolders);
         return vdfRoot.ToJson().Value
-            .SelectMany(i => i)
-            .Select(i => i.ToObject<LibraryFolder>()!);
+            .SelectMany(_ => _)
+            .Select(_ => _.ToObject<LibraryFolder>()!);
     }
 
     private record LibraryFolder
