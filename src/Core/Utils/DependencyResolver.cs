8f9792993c2368e8b94c4f00e080e5961d434710
Paolo Ambrosio <git@paolo.ambrosio.name>
2025-05-18 06:56:58 +0100

    Dependency resolver

---
+++ src/Core/Utils/DependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Immutable;
+
+namespace Core.Utils;
+
+public static class DependencyResolver
+{
+    /// <summary>
+    /// Resolves individual dependencies into transitive ones.
+    /// </summary>
+    /// <param name="individualDependencies">Individual dependencies</param>
+    /// <returns>Transitive dependencies</returns>
+    public static IDictionary<string, IReadOnlySet<string>> Transitive(
+        IDictionary<string, IReadOnlyCollection<string>> individualDependencies)
+    {
+        var transitiveDependencies = new Dictionary<string, IReadOnlySet<string>>();
+
+        IEnumerable<string> TransitiveDependencies(string item)
+        {
+            if (transitiveDependencies.TryGetValue(item, out var td))
+            {
+                return td;
+            }
+
+            if (!individualDependencies.TryGetValue(item, out var id))
+            {
+                return ImmutableHashSet<string>.Empty;
+            }
+
+            td = id.Concat(id.SelectMany(TransitiveDependencies)).ToHashSet();
+            transitiveDependencies.Add(item, td);
+            return td;
+        }
+
+        foreach (var item in individualDependencies.Keys)
+        {
+            TransitiveDependencies(item);
+        }
+        return transitiveDependencies;
+    }
+}
---
+++ tests/Core.Tests/Utils/DependencyResolverTest.cs
@@ -0,0 +1,40 @@
+﻿using Core.Utils;
+using FluentAssertions;
+
+namespace Core.Tests.Utils;
+
+[UnitTest]
+public class DependencyResolverTest
+{
+    [Fact]
+    public void Transitive_IgnoresMissingDependencies()
+    {
+        DependencyResolver
+            .Transitive(new Dictionary<string, IReadOnlyCollection<string>>
+            {
+                ["A"] = new HashSet<string>(["A1"])
+            }).Should().BeEquivalentTo(new Dictionary<string, IReadOnlySet<string>>
+            {
+                ["A"] = new HashSet<string>(["A1"])
+            });
+    }
+
+    [Fact]
+    public void Transitive_FollowsDependencyTree()
+    {
+        DependencyResolver
+            .Transitive(new Dictionary<string, IReadOnlyCollection<string>>
+            {
+                ["A"] = new HashSet<string>(["B"]),
+                ["B"] = new HashSet<string>(["C", "D"]),
+                ["C"] = new HashSet<string>(["E"]),
+                ["D"] = new HashSet<string>(["E"])
+            }).Should().BeEquivalentTo(new Dictionary<string, IReadOnlySet<string>>
+            {
+                ["A"] = new HashSet<string>(["B", "C", "D", "E"]),
+                ["B"] = new HashSet<string>(["C", "D", "E"]),
+                ["C"] = new HashSet<string>(["E"]),
+                ["D"] = new HashSet<string>(["E"])
+            });
+    }
+}
